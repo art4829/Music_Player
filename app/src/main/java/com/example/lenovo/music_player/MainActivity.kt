bc1d60a6e875ee9018db2e2c9be79d9d63c1916f
art4829 <art4829@g.rit.edu>
2018-07-08 22:38:02 +0545

    initial UI for app

--- app/src/main/java/com/example/lenovo/music_player/MainActivity.kt
+++ app/src/main/java/com/example/lenovo/music_player/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.lenovo.music_player
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
