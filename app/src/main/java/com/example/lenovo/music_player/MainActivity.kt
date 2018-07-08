6a01b140b9cb0c9b87069d5f0c22188dfb078495
art4829 <art4829@g.rit.edu>
2018-07-09 00:32:36 +0545

    layout

--- app/src/main/java/com/example/lenovo/music_player/MainActivity.kt
+++ app/src/main/java/com/example/lenovo/music_player/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.lenovo.music_player
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var albumArt: ImageView? = null
+
+    var playButton: ImageButton? = null
+    var shuffleButton: ImageButton? = null
+
+    var songTitle: TextView? = null
+    var songArtist : TextView? = null
+
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+
+            //Ask for permission
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),0)
+        }else{
+            createPlayer()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            createPlayer()
+        }else{
+            longToast("Permission not granted. Shutting down.")
+            finish()
+        }
     }
 }
